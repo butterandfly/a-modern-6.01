a39562d0624eb3b8a75a70742373f19905c5e63d
butterandfly <zero.hero.lin@gmail.com>
2023-02-18 05:06:41 +0800

    Added SimpleParkingGate

--- Week03/sm_examples.py
+++ Week03/sm_examples.py
@@ -31,6 +31,65 @@ def UpDown_example():
     up_down = UpDown()
     up_down.transduce(['u', 'u', 'd'], verbose=True)
  
+class Delay(StateMachine):
+    def __init__(self, start_state):
+        self.start_state = start_state
+
+    def get_next_values(self, state, inp):
+        return (inp, state)
+
+def Delay_example():
+    delay = Delay(0)
+    delay.transduce([1, 2, 3], verbose=True)
+
+class Average2(StateMachine):
+    start_state = 0
+    def get_next_values(self, state, inp):
+        return (inp, (state + inp) / 2)
+
+def Average2_example():
+    average2 = Average2()
+    average2.transduce([1, 2, 3], verbose=True)
+
+class SumLast3(StateMachine):
+    start_state = (0, 0)
+    def get_next_values(self, state, inp):
+        return ((state[1], inp), state[0] + state[1] + inp)
+
+def SumLast3_example():
+    sum_last3 = SumLast3()
+    sum_last3.transduce([1, 2, 3, 4, 5, 6], verbose=True)
+
+class SimpleParkingGate(StateMachine):
+    start_state = 'waiting'
+
+    def generate_output(self, state):
+        if state == 'raising':
+            return 'raise'
+        elif state == 'lowering':
+            return 'lower'
+        else:
+            return 'nop'
+
+    def get_next_values(self, state, inp):
+        gate_position, car_at_gate, car_just_exited = inp
+        next_state = None
+
+        if state == 'waiting' and car_at_gate:
+            next_state = 'raising'
+        elif state == 'raising' and gate_position == 'top':
+            next_state = 'raised'
+        elif state == 'raised' and car_just_exited:
+            next_state = 'lowering'
+        elif state == 'lowering' and gate_position == 'bottom':
+            next_state = 'waiting'
+        else:
+            next_state = state
+
+        return (next_state, self.generate_output(next_state))
 
 ABCAcceptor_example()
 UpDown_example()
+Delay_example()
+Average2_example()
+SumLast3_example()
